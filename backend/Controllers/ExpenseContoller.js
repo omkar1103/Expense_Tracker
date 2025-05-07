17d53727cd8878a2ee9cc48da75d03b0198637d1
omkar1103 <115880514+omkar1103@users.noreply.github.com>
2025-05-07 17:30:24 +0530

    Developed fully backend by testing the API for fetch delete and add expense

--- backend/Controllers/ExpenseContoller.js
+++ backend/Controllers/ExpenseContoller.js
@@ -30,14 +30,53 @@ const addExpense= async(req,res)=>{
 }
 
 
-const deleteExpense=(req,res)=>{
-    res.send('deleteExpense');
+const deleteExpense=async(req,res)=>{
+    const {_id}=req.user;
+    const {expesneId}=req.params;
+    try {
+        const userData=await UserModel.findByIdAndUpdate(
+            _id,{
+                $pull:{expenses:{_id:expesneId}}
+            },
+            {new :true}
+        ) ;
+        return res.status(200).json({
+            message:"Expense Deleted successfully",
+            success:true,
+            data:userData?.expenses
+        })
+        
+    } catch (err) {
+        return res.send(500).json({
+            message:"Something went wrong",
+            error:err,
+            success:false
+        })
+        
+    }
 }
 
 
 
-const fetchExpense=(req,res)=>{
-    res.send('fetchExpense');
+const fetchExpense=async(req,res)=>{
+    const body=req.body;
+    const {_id}=req.user;
+    try {
+        const userData=await UserModel.findById(_id).select('expenses');
+        return res.status(200).json({
+            message:"Fetched Expenses successfully",
+            success:true,
+            data:userData?.expenses
+        })
+        
+    } catch (err) {
+        return res.send(500).json({
+            message:"Something went wrong",
+            error:err,
+            success:false
+        })
+        
+    }
 }
 
 module.exports={
